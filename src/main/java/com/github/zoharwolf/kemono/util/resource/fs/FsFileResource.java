dc02b29f925bdf9ab0c51b16ad00e47bc1d20562
mk124 <mk124a@gmail.com>
2012-05-10 01:41:32 +0800

    添加空的DirResource getParent()以避免错误，等待大白狗兄来实现之-.-。
    Signed-off-by: mk124 <mk124a@gmail.com>

--- src/main/java/com/github/zoharwolf/kemono/util/resource/fs/FsFileResource.java
+++ src/main/java/com/github/zoharwolf/kemono/util/resource/fs/FsFileResource.java
@@ -5,58 +5,66 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import com.github.zoharwolf.kemono.util.resource.DirResource;
 import com.github.zoharwolf.kemono.util.resource.FileResource;
 
 /**
  * 从文件系统中获取文件资源类
+ * 
  * @author zohar
- *
+ * 
  */
+
 public class FsFileResource implements FileResource
 {
-    String path;
-    
-    public FsFileResource(String path)
-    {
-        this.path = path;
-    }
-
-    @Override
-    public String getPath()
-    {
-        return path;
-    }
-
-    @Override
-    public int getSize()
-    {
-        File file = new File(path);
-        return (int) file.length();
-    }
-
-    @Override
-    public InputStream getInputStream()
-    {
-        File file = new File(path);
-        FileInputStream fis = null;
-        try
-        {
-            fis = new FileInputStream(file);
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        return fis;
-        
-    }
-    
-    @Override
-    public String toString()
-    {
-        return path;
-    }
+	private String path;
 
 
+	public FsFileResource(String path)
+	{
+		this.path = path;
+	}
 
+	@Override
+	public String getPath()
+	{
+		return path;
+	}
+	
+	@Override
+	public DirResource getParent()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSize()
+	{
+		File file = new File(path);
+		return (int) file.length();
+	}
+
+	@Override
+	public InputStream getInputStream()
+	{
+		File file = new File(path);
+		FileInputStream fis = null;
+		try
+		{
+			fis = new FileInputStream(file);
+		}
+		catch( FileNotFoundException e )
+		{
+			e.printStackTrace();
+		}
+		
+		return fis;
+	}
+
+	@Override
+	public String toString()
+	{
+		return path;
+	}
 }
