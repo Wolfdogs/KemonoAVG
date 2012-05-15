0831991be786d9e02710f2539ffa0994d9496baf
mk124 <mk124a@gmail.com>
2012-05-15 11:03:27 +0800

    重构ResourceManager的一部分代码。
    Signed-off-by: mk124 <mk124a@gmail.com>

--- src/main/java/com/github/zoharwolf/kemono/util/resource/ResourceManager.java
+++ src/main/java/com/github/zoharwolf/kemono/util/resource/ResourceManager.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	 http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -41,17 +41,6 @@ public final class ResourceManager
 	
 	
 	/**
-	 * 获取资源的方法。
-	 * 
-	 * @param resName 这样的格式： 资源类型1缩写://资源1文件(夹);资源类型1缩写://资源1文件(夹);... 后面的资源优先级高于前边的。支持通配符*
-	 * @return
-	 */
-	public static DirResource getResource( String resName )
-	{
-		return new MultiDirResource( getDirs( resName ) );
-	}
-	
-	/**
 	 * 增加支持的资源类型。
 	 * 
 	 * @param name 资源类型缩写
@@ -62,12 +51,31 @@ public final class ResourceManager
 		RES_TYPES.put(name, cls);
 	}
 	
-	private static List<DirResource> getDirs( String resName )
+	/**
+	 * 获取资源的方法。
+	 * 
+	 * @param resName 这样的格式： 资源类型1缩写://资源1文件(夹);资源类型1缩写://资源1文件(夹);... 后面的资源优先级高于前边的。支持通配符*
+	 * @return
+	 */
+	public static DirResource getResource( String resName )
+	{
+		try
+		{
+			return new MultiDirResource( getDirs(resName) );
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private static List<DirResource> getDirs( String resLocator ) throws FileNotFoundException
 	{
 		List<DirResource> dirs = new ArrayList<>();
-		String[] resources = resName.split(";");
+		String[] resources = resLocator.split(";");
 		
-		for ( int i=resources.length-1; i>=0; i--)
+		for( int i=resources.length-1; i>=0; i-- )
 		{
 			String oneRes = resources[i];
 			String[] typeAndRes = oneRes.split("://");
@@ -79,74 +87,47 @@ public final class ResourceManager
 		return dirs;
 	}
 	
-	private static Collection<? extends DirResource> getAllMatchedResources( String type, final String path )
+	private static Collection<? extends DirResource> getAllMatchedResources( String type, final String path ) throws FileNotFoundException
 	{
-		List<DirResource> matchedRes = new ArrayList<>();
+		List<DirResource> matches = new ArrayList<>();
 		
 		// 取到文件所在的父目录
 		int lastIndex = path.lastIndexOf( '/' );
-		String parent = "";
-		if( lastIndex == -1 )
-		{
-			parent = ".";
-		}
-		else
-		{
-			parent = path.substring( 0, lastIndex );
-		}
+		String parent = ".";
+		
+		if( lastIndex != -1 ) parent = path.substring( 0, lastIndex );
 
 		// 取出父目录下所有与path匹配的资源
-		File[] files = new File(parent).listFiles( new FileFilter()
-		{
-			@Override
-			public boolean accept( File pathname )
+		File[] files = new File(parent).listFiles(
+			new FileFilter()
 			{
-				return match( path, pathname.getName() );
-			}
-		} );
+				@Override
+				public boolean accept( File pathname )
+				{
+					return match( path, pathname.getName() );
+				}
+			} );
 
-		// 没找到匹配资源时会显示警告
-		if( files.length <= 0 )
+		// 没找到匹配资源时抛出异常
+		if( files.length == 0 ) throw new FileNotFoundException( "No matched resource found in " + path );
+
+		for( File file : files )
 		{
+			if( ! RES_TYPES.containsKey(type) ) throw new UnsupportedOperationException( "This type is not valid: " + type );
+			
 			try
 			{
-				throw new FileNotFoundException( "WARNING： No matched resource found in " + path );
+				Class<? extends DirResource> cls = RES_TYPES.get(type);
+				DirResource dirRes = cls.getConstructor(File.class).newInstance( file );
+				matches.add( dirRes );
 			}
-			catch( FileNotFoundException e )
+			catch( InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e )
 			{
 				e.printStackTrace();
 			}
 		}
 
-		for( File file : files )
-		{
-			if ( RES_TYPES.containsKey(type) )
-			{
-				try
-				{
-					Class<? extends DirResource> cls = RES_TYPES.get(type);
-					DirResource dirRes = cls.getConstructor(File.class).newInstance( file );
-					matchedRes.add( dirRes );
-				}
-				catch( InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e )
-				{
-					e.printStackTrace();
-				}
-			}
-			else
-			{
-				try
-				{
-					throw new Exception( "WARNING： This type is not valid: " + type );
-				}
-				catch( Exception e )
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-
-		return matchedRes;
+		return matches;
 	}
 	
 	private static boolean match( String condition, String target )
